8858cc3b63a458af01916e78b2c69203d9689e50
Luis Bezerra <luisbezerra.work@gmail.com>
2025-06-04 16:08:57 -0300

    add flake8 to project

--- api/main.py
+++ api/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
 
-
-app = FastAPI()
+app = FastAPI(
+    title="Resume Analyzer",
+    description="""
+        An intelligent Python-based application to automate résumé analysis using OCR and LLMs.
+        It accepts multiple files (PDFs or images), extracts text via OCR, generates summaries, and answers custom queries about candidate profiles — all integrated with auditable logging via MongoDB.
+    """,  # noqa: E501
+)
